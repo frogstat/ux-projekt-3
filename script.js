faad7b119abba862d921ebfbab7e2fad0f5afa23
Adam <adamgareb@hotmail.com>
2026-04-24 11:36:43 +0200

    Working on bookings style

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ import { createApp, ref, onMounted } from "vue";
 const body = document.getElementsByTagName('body')[0];
 
 const app = {
+    data(){
+        return{
+            baseStart: parseDate("2026-03-30")
+        }
+    },
+
+    computed: {
+        visibleDays() {
+          return Array.from({ length: 28 }, (_, i) =>
+            addDays(this.baseStart, i)
+          );
+    },
+
     setup() {
         const jsonResults = ref([]);
 
@@ -31,12 +44,39 @@ const app = {
             });
         }
 
+        function barStyle(job) {
+            const from = job.from;
+            const to = job.to;
+            const height = job.percentage === 100 ? 2 : 1;
+            const diffDays = getDateDiff(from, to);
+
+            console.log("From: " + from + ". To: " + to);
+            console.log(diffDays);
+            return {
+                height: height
+            }
+        }
+
+        function getDateDiff(from, to) {
+            const fromDate = new Date(from);
+            const toDate = new Date(to);
+
+            const diffDate = toDate - fromDate;
+            const diffDays = Math.ceil(diffDate / (1000 * 60 * 60 * 24));
+
+            return diffDays;
+        }
+
+        function visibleProjects(job) {
+            
+            return
+        }
+
         onMounted(() => {
             createListOfNames();
         });
 
-        return { jsonResults };
-    }
-}
 
-createApp(app).mount("#app")
+        return { jsonResults, barStyle };
+    }
+}.mount("#app")}
